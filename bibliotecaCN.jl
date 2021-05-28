46c7711e6e04a63e789c6d49cf94503af600d1ea
viniciuslettieri <viniciuslettieri@yahoo.com.br>
2021-05-28 15:39:01 -0300

    Criação do arquivo com as funções da biblioteca

--- bibliotecaCN.jl
+++ bibliotecaCN.jl
@@ -1,3 +1,20 @@
-function foo()
-    println("Olá Brasil!")
+function eliminacao_gaussiana(A,b,n)
+    for i = 1:n-1
+        for j = i+1:n
+            mult = A[j,i]/A[i,i]
+            A[j,:] = A[j,:] - mult*A[i,:]
+            b[j] = b[j] - mult*b[i]
+        end
+    end
+
+    return A,b
+end
+
+function main()
+    A=[2 4 3;4 10 9;6 18 22]
+    b=[9; 23; 46]
+    T,c = eliminação_gaussiana(A,b,3)
+
+    println(T)
+    println(c)
 end
